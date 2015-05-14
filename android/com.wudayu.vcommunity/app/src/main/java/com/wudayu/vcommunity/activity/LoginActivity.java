3d0ee92db865ea6e2e3e87a6684e7b3e5f4dbbda
David Wu <wudayu@gmail.com>
2015-05-14 21:52:17 +0800

    edit version

--- android/com.wudayu.vcommunity/app/src/main/java/com/wudayu/vcommunity/activity/LoginActivity.java
+++ android/com.wudayu.vcommunity/app/src/main/java/com/wudayu/vcommunity/activity/LoginActivity.java
@@ -7,7 +7,6 @@ import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.ImageView;
-import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import com.nineoldandroids.view.ViewHelper;
